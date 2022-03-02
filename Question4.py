5b15d36f84ea6104b1e96474cfd90c2d36f9366d
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-02 19:19:12 +0530

    Question 4

--- Question4.py
+++ Question4.py
@@ -1,3 +1,4 @@
 a=input("Enter a sentence")
 b=a.lower()
 print("Count of a = ",b.count("a"),"\n","Count of e = ",b.count("e"),"\n","Count of i = ",b.count("i"),"\n","Count of o = ",b.count("o"),"\n","Count of u = ",b.count("u"))
+# This will print the count of each vowel from the given sentence
